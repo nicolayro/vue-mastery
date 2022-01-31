1c6df694c436caa57f99efc5d4a05dcdcca9622a
Nicolay Roness <nicroness@gmail.com>
2022-01-31 15:38:20 +0100

    Implemented pagination

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,31 +2,65 @@
   <h1>Events for good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <div class="pagnation">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Previous</router-link
+      >
+
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next &#62;</router-link
+      >
+    </div>
   </div>
 </template>
 
 <script>
 import EventCard from "@/components/EventCard.vue";
 import EventService from "@/services/EventService.js";
+import { watchEffect } from "vue";
 
 export default {
   name: "EventList",
+  props: ["page"],
   components: {
     EventCard,
   },
   data() {
     return {
       events: [],
+      eventsPerPage: 2,
+      totalEvents: 0,
     };
   },
-  async created() {
-    try {
-      const response = await EventService.getEvents();
-      console.log(response.data);
-      this.events = response.data;
-    } catch (err) {
-      console.log(err);
-    }
+  created() {
+    watchEffect(async () => {
+      this.events = null;
+      try {
+        const response = await EventService.getEvents(
+          this.eventsPerPage,
+          this.page
+        );
+        this.events = response.data;
+        this.totalEvents = response.headers["x-total-count"];
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  },
+  computed: {
+    hasNextPage() {
+      var totalPages = Math.ceil(this.totalEvents / 2);
+      return this.page < totalPages;
+    },
   },
 };
 </script>
@@ -36,5 +70,17 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagnation {
+  display: flex;
+  justify-content: space-between;
+  width: 290px;
+}
+
+.pagnation a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
 }
 </style>
